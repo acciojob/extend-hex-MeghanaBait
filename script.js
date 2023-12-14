5b291db4208e473614c15e4c0c4b01d65c50dbf7
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-14 08:34:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i = shortHex.length-3;
+	let iscapital=false;
+	let ans = "#";
+	while(i < shortHex.length){
+		let code= shortHex[i].chatCodeAt(0);
+		if(code >= 65 &&code <=90){
+			iscapital=true;
+		}
+		ans += shortHex[i]+shortHex[i];
+		i++;
+	}
+	return iscapital ? ans.toUpperCase() : ans;
 };
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+alert(extendHex(shortHex)); 
