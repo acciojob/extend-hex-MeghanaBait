a401123b91bb4236c645749bbc4d0ac44f9f5a40
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-14 08:40:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const extendHex = (shortHex) => {
-  // write your code here
-	let i = shortHex.length-3;
-	let iscapital=false;
-	let ans = "#";
-	while(i < shortHex.length){
-		let code= shortHex[i].charCodeAt(0);
-		if(code >= 65 &&code <=90){
-			iscapital=true;
-		}
-		ans += shortHex[i]+shortHex[i];
-		i++;
-	}
-	return isCapital ? ans.toUpperCase() : ans;
+    let i = shortHex.length - 3;
+    let isCapital = false;
+    let ans = "#";
+
+    while (i < shortHex.length) {
+        let code = shortHex[i].charCodeAt(0);
+
+        if (code >= 65 && code <= 90) {
+            isCapital = true;
+        }
+
+        ans += shortHex[i] + shortHex[i];
+        i++;
+    }
+
+    return isCapital ? ans.toUpperCase() : ans;
 };
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex)); 
+alert(extendHex(shortHex));
